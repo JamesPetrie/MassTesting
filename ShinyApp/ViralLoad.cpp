71008069987154a78560dc57e0e5d9c1848996bf
James Petrie <james.petrie@protonmail.com>
2023-10-16 12:38:30 +0100

    code cleanup

--- ShinyApp/ViralLoad.cpp
+++ ShinyApp/ViralLoad.cpp
@@ -6,101 +6,68 @@ using namespace Rcpp;
 #include <memory>
 
 
-// done - step 1: run model from R, plot cases per day
-// done - step 2: add risk of transmission based on viral load
-// done - step 3: verify contact tracing working
-// done - step 4: make hourly instead of daily
-// step 5: add checks at each timestep time since last test, symptoms,  contact tracing, queue of test results
-// step 6: add parameters for success rate of various steps
-// step 7: change strategy based on time and case count
-// step 8: return number of undetected infectious days, tests per person, number quarantined, number isolated
-// optional: make it faster?
-
-
-
-
-
-//start of MT functions 
-//each person can be normal, quarantine or isolation
 enum State {NORMAL, QUARANTINE, ISOLATION};
 
-
-
-// object storing info for a person. Contains:
-// day of infection
-// list of infected contacts (implicitly keep track of uninfected contacts based on SAR)
+// object storing infection timing info for a person. 
 // uses value of INT_MAX to indicate state isn't reached
-struct Case { // for a case we want to to know when they were infected, earliest time they received a positive test, 
+struct Case { 
   int hourInfected = INT_MAX;
-  int hourInfectionDetected = INT_MAX;
-  int hourSymptoms = INT_MAX; //hour they get symptoms
-  int hourTraced = INT_MAX; // time they get traced
-  int hourLastTested = INT_MIN; // time they were last tested 
+  int hourIso = INT_MAX; 
+  int hourTraceContacts = INT_MAX;
+  int hourTraced = INT_MAX; 
   int infectedBy = -1; // index of the person who infected them
   int totalTransmissions = 0;
-  std::vector<int> contacts; // vector of pointers to contact
- State state = NORMAL; 
-  bool adheresToTesting; // see if they have adhered to testing
+  std::vector<int> contacts; // vector of indices of people infected
+  State state = NORMAL; 
 
-  //constructor for case - have when they were infected, test period ( how often they test), params from mass testing
   Case(int infectedHour,  int infectorId, const NumericVector params): hourInfected(infectedHour), infectedBy(infectorId){
-    if( runif(1)[0] < params["ProbTestSymptoms"]){ //randomly sample whether this is a person that both develops symptoms and tests because of them
-      hourSymptoms = hourInfected + rexp(1, 1.0/(5.0*24))[0]; // if they do detect symptoms, we record when they started having noticable symptoms
-
+    if(params["ProbIso"] > runif(1)[0]){
+      hourIso = hourInfected + rexp(1, 1.0/(5.0*24))[0]; // if they do detect symptoms, we record when they started having noticable symptoms
     }
-
+    
+    if(params["IsoAndTraceSameTime"]){
+      hourTraceContacts = hourIso;
+    }else{
+      if(params["ProbIso"] > runif(1)[0]){
+        hourTraceContacts = hourInfected + rexp(1, 1.0/(5.0*24))[0]; // if they do detect symptoms, we record when they started having noticable symptoms
+      }
+    }
   } 
 
-  void recordTransmission(){
-    totalTransmissions++;
+  void recordTransmissions(int numTransmissions){
+    totalTransmissions += numTransmissions;
   }
   
   void addContact(int contactIndex){
     contacts.push_back(contactIndex);
-    totalTransmissions++;
-  } //people this person infected 
+  }
 
-
-  void getTraced(int hour, const NumericVector params ){
-    std::cout << hour;
-    //std::cout << "traced";
+  void getTraced(int hour){
     hourTraced = hour;
-  } //time u were contact traced but we need to add a delay because it's not immediate
-
-  // todo: create tests queue
-  // create state enum
+    if(state == NORMAL){ 
+      state = QUARANTINE;
+    }
+  } 
+  
   int update(int hour, std::vector<Case>& cases, const NumericVector params ){
- 
-    // processing test and decide what to do with people 
-    // if no infection detected yet, check for test results
-      if(hour == hourSymptoms){ // if you don't have your test results back yet 
-        state = ISOLATION; // you isolate if the test is positive and record when they got infected 
-        hourInfectionDetected = hour;
-        //notify contacts
-        //std::cout << contacts.size() << " ";
-        for(int contactIndex : contacts){ //for each of these contacts this person infected, we sample if they got traced based on prob they got traced
-          if (params["ProbTracedGivenInfectorDetected"] > runif(1)[0]){ // < params["ProbTracedGivenInfectorDetected"]){
-            cases[contactIndex].getTraced(hour, params);
-          }
+    if(hour == hourIso){
+      state = ISOLATION; 
+    }
+    if(hour == hourTraceContacts){
+      //notify contacts
+      for(int contactIndex : contacts){ 
+        if (params["ProbTrace"] > runif(1)[0]){ 
+          cases[contactIndex].getTraced(hour);
         }
-        
-
+      }
     }
-
-    //choosing when to get tested and quarantine 
-    // check whether they should get tested
-    if(state == NORMAL && hour >=  hourTraced){ // they are infected but not aware, everyone modelled is infected 
-        state = QUARANTINE; // they were contact traced but they dont know they were positive yet 
-    }
-
     
     // check for onward transmissions
     if(state == QUARANTINE || state == ISOLATION){
       return(0);
-    }else{ // if the time is not yet at the point when they are not infectious, they could still infect other people 
+    }else{ 
       double lambda = 1/(24*5.0);
-      double R0 = 3;
-      double transmitRate = R0*lambda*exp(-lambda*(hour - hourInfected));
+      double transmitRate = params["R0"]*lambda*exp(-lambda*(hour - hourInfected));
 
       int numTransmissions = rpois(1, transmitRate)[0]; // number of transmissions -> pois takes rate give int 
       return(numTransmissions);
@@ -110,17 +77,12 @@ struct Case { // for a case we want to to know when they were infected, earliest
 
 
 
-
-
-
-
 // input: set of parameters describing disease and population
 // return: time series with daily true and estimated values for different states
 // [[Rcpp::export]]
 Rcpp::DataFrame branchingModel(int endDay, int maxSize, const NumericVector params){
   std::vector<Case> cases; // vector of cases but empty 
 
-  
   // add 10 initial cases
   for(int i =0;i<10;i++){ // 0 is the hour they were infected, -1 is the index 
     Case initialCase = Case(0,  -1,  params);
@@ -129,58 +91,44 @@ Rcpp::DataFrame branchingModel(int endDay, int maxSize, const NumericVector para
 
   int simulationEndHour = endDay*24;
   // iterate over number of days. For each day generate new cases
-  //for(int hour = 0;hour<endDay*24;hour++){ // ingnore for now- checks if there is outbreak and change the test period to outbreak test period 
-  for(int hour = 0;hour< simulationEndHour;hour++){
+   for(int hour = 0;hour< simulationEndHour;hour++){
 
     size_t size = cases.size(); // get size before through the loop because we are adding to the list ( number of cases) as we go. 
     for (size_t i = 0; i < size; ++i){ // go through each cases
-      
-      //how many that case transmits to at that hour 
-      int numTransmissions = cases[i].update(hour, cases, params); //call update date function to get how many each erson  transmits to and more info
+      int numTransmissions = cases[i].update(hour, cases, params); //call update date function to get how many each person transmits to
       
       if(numTransmissions > 0){
-        //std::cout << "Case:" << i << ", num transmissions: " << numTransmissions << ", hour = " <<hour;
+        cases[i].recordTransmissions(numTransmissions);
         for(int j = 0; j < numTransmissions; j++){ // we create new cases and keeps track of who infected, when infected 
           if(cases.size() < maxSize - 1){
             Case newCase = Case(hour, i, params);
             cases.push_back(newCase);// for each transmission, create new case and add to end of vector
             cases[i].addContact(cases.size()-1);
-          }else{
-            cases[i].recordTransmission();
           }
-  
         }
       }
-
     }
   }
 
-
+  // At the end of simulation - record the info for each case
   size_t numCases = cases.size();
   std::vector<int> infectedHours(numCases);
-  std::vector<int> detectedHours(numCases);
   std::vector<int> tracedHours(numCases);
   std::vector<int> infectorIds(numCases);
   std::vector<int> ids(numCases);
   std::vector<int> transmissionCounts(numCases);
-  std::vector<int> endHours(numCases);
 
-//for each of the cases we are making the final data frame
-//we keep track of who each case is infected by but we do keep track of how many people erach case infected NumInfected
   for(int i =0; i< numCases; i++){
     infectedHours[i] = cases[i].hourInfected;
-    detectedHours[i] = cases[i].hourInfectionDetected;
     tracedHours[i] = cases[i].hourTraced;
     infectorIds[i] = cases[i].infectedBy;
     ids[i] = i;
     transmissionCounts[i] = cases[i].totalTransmissions;
-    endHours[i] = simulationEndHour;
   }
 
   // add all case data to caseLog dataframe
-  Rcpp::DataFrame df = DataFrame::create( _["InfectedHour"] = infectedHours, _["DetectedHour"] = detectedHours, _["TracedHour"] = tracedHours,
-                                          _["InfectorId"] = infectorIds, _["Id"] = ids, _["NumInfected"] = transmissionCounts, _["SimulationEndHour"] = endHours);
-
+  Rcpp::DataFrame df = DataFrame::create( _["InfectedHour"] = infectedHours,  _["TracedHour"] = tracedHours,
+                                          _["InfectorId"] = infectorIds, _["Id"] = ids, _["NumInfected"] = transmissionCounts);
 
   return(df);
 }
--- ShinyApp/test_MT_CT.R
+++ ShinyApp/test_MT_CT.R
@@ -1,165 +1,34 @@
-
-#test file for MT & CT
-folder =  "~/MassTesting/ShinyApp/" # "/Users/orayasrim/Documents/MassTest/MassTesting/ShinyApp/" #
-Rcpp::sourceCpp(paste0(folder, "ViralLoad.cpp"))
-source(paste0(folder, "buildFigures.R"))
 library(boot)
-#plots (daily testing) another will have testing every 4 days for example ( noraml test period and outbreak test period)
-#default 1 day and additional testing with 0 or 2 days ( tracing delay)
-#fraction traced along x axis () ( probtraced given )
-#prob detect symptoms (depends on disease) -> can set to 0 to ignore -> modify based on disease
-#time to peak to symptoms -> modify based on disease
-#days to peak -> depends on disease
-#contact per day is 13, test delay = 10 hours, fraciso = 100 or 1, frac test = 0.9 to do thihnk about turn off contact tracing for people who dont test, 
-#maxprobtranmit per exposure = 0.3, relative decline slope = 1, max days after peak. = 20 , mask effect = 0, minlogpcr = +2.5, initial log load = -2.5
+library(data.table)
+library(plyr)
 
-# #base
-# normalTestPeriod = 1 #adjust scenario
-# outbreakTestPeriod = 1 #adjust scenario
-# tracingDelay = 0 #adjust scenario
-# fractionTraced = 0.1 #adjust this one
-# probDetectSymptoms = 0 #adjust based on disease
-# timeFromPeaktoSymptoms = 1 #adjust based on disease
-# daysToPeak = 6 #adjust based on disease
-# contactsPerDay = 13
-# testDelay = 10
-# fracIso = 1
-# fracTest = 0.90
-# maxProbTransmitPerExposure = 0.30
-# relativeDeclineSlope = 1
-# maxDaysAfterPeak = 20
-# maskEffect = 0
-# minLogPCRViralLoad = 2.5
-# initialLogLoad = -2.5
-# R0 = 3 # adjust based on disease 
+Rcpp::sourceCpp("~/MassTesting/ShinyApp/ViralLoad.cpp")
 
-#example diseases
-# #influenza
-#   probTest = .50 #adjust scenario
-#   normalTestPeriod = 4 #adjust scenario
-#   outbreakTestPeriod = 4 #adjust scenario
-#   tracingDelay = 0 #adjust scenario
-#   #fractionTraced = 0.1 #adjust this one
-#   probDetectSymptoms = 0.693*probTest #adjust based on disease
-#   timeFromPeaktoSymptoms = -2 #adjust based on disease
-#   daysToPeak = 3 #adjust based on disease
-#   contactsPerDay = 13
-#   testDelay = 10
-#   fracIso = 1
-#   fracTest = 0.90
-#   maxProbTransmitPerExposure = 0.30
-#   relativeDeclineSlope = 1
-#   maxDaysAfterPeak = 20
-#   maskEffect = 0
-#   minLogPCRViralLoad = 2.5
-#   initialLogLoad = -2.5
-#   R0 = 2.0 #adjust based on disease
+numOutbreaks = 100
+endDay = 300
+maxSize = 1000
 
-# #SARS-CoV
-# probTest = .80 #adjust scenario
-# normalTestPeriod = 1 #adjust scenario
-# outbreakTestPeriod = 1 #adjust scenario
-# tracingDelay = 0 #adjust scenario
-# fractionTraced = 0.1 #adjust this one
-# probDetectSymptoms = 0.925*probTest #adjust based on disease
-# timeFromPeaktoSymptoms = -5 #adjust based on disease
-# daysToPeak = 7 #adjust based on disease
-# contactsPerDay = 13
-# testDelay = 10
-# fracIso = 1
-# fracTest = 0.90
-# maxProbTransmitPerExposure = 0.30
-# relativeDeclineSlope = 1
-# maxDaysAfterPeak = 20
-# maskEffect = 0
-# minLogPCRViralLoad = 2.5
-# initialLogLoad = -2.5
-# R0 = 2.4 # adjust based on disease
-
-#add covid-19 
-
-minLogPCRViralLoad = 2.5
-initialLogLoad = -2.5
-maxProbTransmitPerExposure = 0.30
-relativeDeclineSlope = 1
-contactsPerDay = 13
-precision = 2.0
-
-
-
-meanfun <- function(data, i){
-  d <- data[i, ]
-  return(mean(d))   
-}
-
-
-test_period_list <- c(1,2,4,8,16)
-
-disease_list <- c("1918 Influenza", "SARS-CoV-1","SARS-CoV-2")
-#disease_list <- c("test")
-
-fractionTraced_list <- seq(0, 0.9, by = 0.1)
-
-timepeak_list <- seq(1,12,2)
-prob_test_list <- c(0.10, 0.50, 0.80, 0.90) 
-frac_iso_list <- c(.10,.50,.90) #remove 100% for figure B4
-
-runAndComputeRe = function(numOutbreaks, endDay, maxSize, diseaseName, R0, timeToPeak, timeFromPeaktoSymptoms,  probTestSymptoms, 
-                           testPeriod, tracingDelay, fractionTraced, testDelay, fracIso, fracTest ){
-  params = c(
-    normalTestPeriod = testPeriod,
-    outbreakTestPeriod = testPeriod ,
-    ContactTracingDelay = tracingDelay ,
-    ProbTracedGivenInfectorDetected = fractionTraced,
-    ProbTestSymptoms = probTestSymptoms,
-    timeFromPeakToSymptoms = timeFromPeaktoSymptoms,
-    timeToPeak = timeToPeak,
-    timeFromPeakTo0 = timeToPeak, 
-    #timeFromPeakTo0 = 24*5,
-    maxTimeAfterPeak= 24*30,
-    contactsPerHour = contactsPerDay/24, testDelay = testDelay, 
-    fracIso = fracIso, fracQuar = fracIso, fracTest = fracTest, 
-    maxProbTransmitPerExposure = maxProbTransmitPerExposure,
-    #maxTimeAfterPeak = 24*30, 
-    maxTimeAfterPeak = 24*20,
-    maskEffect = 0, minLogPCRViralLoad = minLogPCRViralLoad, initialLogLoad = initialLogLoad, 
-    probTransmitMid = 8.9e6,logLimitOfDetection = minLogPCRViralLoad ,infectHParam = 0.51, 
-    #precision = precision
-    precision = 0.2
-    )
+params = c(ProbTrace = 1,ProbIso = 1, R0 = 2.8, IsoAndTraceSameTime = TRUE)
   
     
-  caseData = rbindlist(llply(1:numOutbreaks, function(i){
-    #browser()
-    dt = data.table(branchingModel(endDay = endDay, maxSize = maxSize, params)) 
-    dt[,RunNumber := i]
-    
-    return(dt)
-  }))
+caseData = rbindlist(llply(1:numOutbreaks, function(i){
+  dt = data.table(branchingModel(endDay = endDay, maxSize = maxSize, params)) 
+  dt[,RunNumber := i]
+  return(dt)
+}))
  
-  caseData[,hourNotInfectious := InfectedHour + 5*24*8] # todo: fix
-  re_filt <- caseData[hourNotInfectious<SimulationEndHour]
-  #for bootstrapping
-  test <- data.frame(re_filt$NumInfected)
-  bo <- boot(test[, "re_filt.NumInfected", drop = FALSE], statistic=meanfun, R=1000)
-  
-  result <- boot.ci(bo, conf=0.95, type = c("basic"))
-  
-  lowerBound <- result$basic[1,4]
-  
-  upperBound <- result$basic[1,5]
-  #end bootstrapping
-  
-  re <- mean(re_filt$NumInfected)
-  
-  x = data.table(Re = re, Disease = diseaseName, FractionTraced = fractionTraced, R0 = R0, testPeriod = testPeriod,timePeak = timeToPeak, minBound = lowerBound, maxBound = upperBound,fractionIso = fracIso)
-  #x = data.table(Re = re, Disease = disease_name, FractionTraced = fractionTraced, R0 = R0, testPeriod = test_period, probTestPositive = prob_test)
-  #x = data.table(Re = re, Disease = disease_name, FractionTraced = fractionTraced, R0 = R0, testPeriod = test_period, fractionIso = frac_iso)
-  
-  return(x)
-}
+caseData = caseData[InfectorId != -1 & (InfectedHour + 5*24*8 < endDay*24)]
+Re <- mean(caseData$NumInfected)
+
+bo <- boot(data.frame(caseData[,NumInfected]), statistic=meanfun, R=1000)
+bootResult <- boot.ci(bo, conf=0.95, type = c("basic"))
+lowerBound <- bootResult$basic[1,4]
+upperBound <- bootResult$basic[1,5]
 
 
-runAndComputeRe(numOutbreaks = 30, endDay = 100, maxSize = 300, diseaseName = "Example", R0 = 3, timeToPeak = 24*5, timeFromPeaktoSymptoms = 0,  probTestSymptoms = 1.0, 
-                testPeriod = 100000*24, tracingDelay = 0, fractionTraced = 1, testDelay = 500, fracIso = 0.9, fracTest = 0)
+result = data.table(Re = Re,  ReLowerBound = lowerBound, ReUpperBound = upperBound,R0 = params["R0"],ProbTrace = params["ProbTrace"], ProbIso = params["ProbIso"], IsoAndTraceSameTime = 1 == params["IsoAndTraceSameTime"])
+
+print(result)
+
+
 
